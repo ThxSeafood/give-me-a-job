8ec34fd528e382d796488c57f912430fd2dee444
as10896 <as10896@gmail.com>
2017-10-22 22:13:14 +0800

    Create api_explore branch

---
+++ lib/api_info.rb
@@ -0,0 +1,28 @@
+require 'http'
+require 'yaml'
+require 'json'
+
+config = YAML.safe_load(File.read('config/secrets.yml'))
+
+def api_path(keywords)
+  'http://www.104.com.tw/i/apis/jobsearch.cfm?kws=' + keywords + '&area=6001001000&order=2&fmt=8&cols=JOB%2CNAME%2Clink&pgsz=2000'
+end
+
+def call_104_url(url)
+  HTTP.get(url)
+end
+
+response = {}
+results = {}
+
+url = api_path('Internet程式設計師')
+response[url] = call_104_url(url)
+
+results['size'] = response[url]["RECORDCOUNT"]
+contents = JSON.parse(response[url].body)
+
+results['size'] = contents['RECORDCOUNT']
+results['contents'] = contents['data']
+
+File.write('spec/fixtures/response.yml', response.to_yaml)
+File.write('spec/fixtures/results.yml', results.to_yaml)
