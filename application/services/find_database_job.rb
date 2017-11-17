84fd2001161e442536bff5fef5c57d1a821e7da2
as10896 <as10896@gmail.com>
2017-11-18 02:03:48 +0800

    add regex func

--- application/services/find_database_job.rb
+++ application/services/find_database_job.rb
@@ -12,8 +12,14 @@ module ThxSeafood
 
     def self.call(input)
       # 這邊得改成回傳jobname裡包含keywords的job entity array (得改寫Jobs，也許要加個方法)
-      jobs = Repository::For[Entity::Job]
-             .find_jobs_by_blank_link()
+      # 後面的else處理是為了應付SQLite不支援regex的情形，所以在local用GET選擇傳回LINK是空的Job資料
+      if environment == 'production'
+        jobs = Repository::For[Entity::Job]
+              .find_jobs_by_using_regexp(input[:jobname])
+      else
+        jobs = Repository::For[Entity::Job]
+              .find_jobs_by_blank_link()
+      end
       if jobs
         Right(Result.new(:ok, jobs))
       else
